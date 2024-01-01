e3734d012caf4f5736b7eeb5a980032c0e496344
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-01 08:33:13 +0530

    improve in weekofday

--- Switch/src/DayOfWeek.java
+++ Switch/src/DayOfWeek.java
@@ -3,18 +3,20 @@ public class DayOfWeek {
        printDayOfWeek(1);
        printDayOfWeek(6);
        printDayOfWeek(0);
+       printDayOfWeek(10);
     }
     public static void   printDayOfWeek(int day){
-        switch (day) {
-            case 0 -> System.out.println("Sunday");
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
+       String dayOfWeek = switch (day) {
+            case 0 -> ("Sunday");
+            case 1 -> ("Monday");
+            case 2 -> ("Tuesday");
+            case 3 -> ("Wednesday");
 
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            default -> System.out.println("Wrong input");
-        }
+            case 4 -> ("Thursday");
+            case 5 -> ("Friday");
+            case 6 -> ("Saturday");
+            default -> ("Invalid day");
+        };
+       System.out.println(day + " stands for " + dayOfWeek);
     }
 }
