8d3508c639aaa3efe2c2418d7392cc4aeb4f77a9
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-01 07:16:24 +0530

    switch

---
+++ Switch/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int switchValue = 5;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("value was 3, 4 or 5.");
+                break;
+            default:
+                System.out.println("No value was found.");
+                break;
+        }
+    }
+}
