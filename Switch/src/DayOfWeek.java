1ca6c9e130b1c9ab1e2ed96834e83a60e36cebcc
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-01 08:27:27 +0530

    dayOfWeek

---
+++ Switch/src/DayOfWeek.java
@@ -0,0 +1,20 @@
+public class DayOfWeek {
+    public static void main(String[] args) {
+       printDayOfWeek(1);
+       printDayOfWeek(6);
+       printDayOfWeek(0);
+    }
+    public static void   printDayOfWeek(int day){
+        switch (day) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            default -> System.out.println("Wrong input");
+        }
+    }
+}
