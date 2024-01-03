7290c912a922f00f00be67f9e00f78fefc0688d2
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-03 19:58:36 +0530

    encapsulation

--- Encapsulation/src/Main.java
+++ Encapsulation/src/Main.java
@@ -4,5 +4,14 @@ public class Main {
         Player akhil = new Player("Akhilesh", 200, "Sword");
         System.out.println("Initial health is " + akhil.healthRemaining());
 
+        Printer printer = new Printer(50,true);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current job pages: %d, Printer Total: %d %n",
+                pagesPrinted,printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current job pages: %d, Printer Total: %d %n",
+                pagesPrinted,printer.getPagesPrinted());
+
     }
 }
