523e77ef1550e9320050e420d1311b50c1feb655
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-04 21:00:05 +0530

    burger class

--- BurgerOOP/src/Main.java
+++ BurgerOOP/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-Item coke = new Item("drink","coke",1.5);
-coke.printItem();
-coke.setSize("LARGE");
-coke.printItem();
+//Item coke = new Item("drink","coke",1.5);
+//coke.printItem();
+//coke.setSize("LARGE");
+//coke.printItem();
+
+        Burger burger = new Burger("regular",4.00);
+        burger.addToppings("BACON","CHEESE","MAYO");
+        burger.printItem();
     }
 }
