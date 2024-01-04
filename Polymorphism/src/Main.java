822501821a3ef1ad5374836b42a6d59b90e36b0b
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-04 10:26:55 +0530

    polymorphism and factory method

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
-        Movie theMovieC = new Comedy("Bhool bhulaiya");
-        theMovieC.watchMovie();
+        Scanner s = new Scanner(System.in);
+        while (true){
+            System.out.print("Enter Type (A fro Adventure, C for comedy,"+
+                    "S for Science Fiction or Q to quit): ");
+            String type = s.nextLine();
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.print("Enter Movie Title");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type,title);
+            movie.watchMovie();
+        }
 
-        Movie theMovieS = new ScienceFiction("Newton");
-        theMovieS.watchMovie();
     }
 }
