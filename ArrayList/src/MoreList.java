7b71a3ed9a933a411503e1bbe70ad93488dca404
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-07 11:27:02 +0530

    arraylist

--- ArrayList/src/MoreList.java
+++ ArrayList/src/MoreList.java
@@ -16,5 +16,20 @@ public class MoreList {
         System.out.println(nextList);
         groceries.addAll(nextList);
         System.out.println(groceries);
+        System.out.println("Third item = " +groceries.get(2));
+        if(groceries.contains("mustard")){
+            System.out.println("List contained mustard");
+        }
+        groceries.add("papaya");
+        groceries.add("yogurt");
+        System.out.println(groceries);
+        System.out.println(groceries.indexOf("yogurt"));
+        System.out.println(groceries.lastIndexOf("yogurt"));
+
+        groceries.remove(1);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+        groceries.removeAll(List.of("apples","eggs"));
+        System.out.println(groceries);
     }
 }
