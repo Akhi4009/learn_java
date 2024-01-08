b4df58204ea8df374e17e7d605904c101df4b3d1
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-08 20:42:42 +0530

    linklist

--- LinkedList/src/dev/LinkListChallenge.java
+++ LinkedList/src/dev/LinkListChallenge.java
@@ -2,6 +2,7 @@ package dev;
 
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance){
     @Override
@@ -24,6 +25,43 @@ public class LinkListChallenge {
         addPlace(placesToVisit, new Place("agra",700));
 
         System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+
+
+        printMenu();
+
+        while (!quitLoop){
+            if(!iterator.hasPrevious()){
+                System.out.println("originating: " + iterator.next());
+                forward = true;
+            }
+            if(!iterator.hasNext()){
+                System.out.println("Final: " + iterator.previous());
+                forward = false;
+            }
+            System.out.println("Enter Value: ");
+            String menuItem = scanner.next().toUpperCase().substring(0,1);
+            switch (menuItem){
+                case "F":
+                    System.out.println("User wants to go forward");
+                    break;
+                case "B":
+                    System.out.println("User wants to go backward");
+                    break;
+                case "M":
+                    printMenu();
+                case "L":
+                    System.out.println(placesToVisit);
+                default:
+                    quitLoop = true;
+                    break;
+            }
+        }
     }
     private static void addPlace(LinkedList<Place> list, Place place){
         if(list.contains(place)){
@@ -48,5 +86,15 @@ public class LinkListChallenge {
             list.add(place);
 
 
+    }
+
+    private static void printMenu(){
+        System.out.println("""
+                Available action (Select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                (Q)uit """);
     }
 }
