804f0efb8dfc9840128c2afe7ddefae7fac1b944
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-03 18:06:20 +0530

    read input using Scanner

--- TakeInput/src/Main.java
+++ TakeInput/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int currentYear = 2024;
@@ -10,8 +12,12 @@ public class Main {
 //        double ageWithPartialYear = Double.parseDouble(userAgeWithPartialYear);
 //        System.out.println("The user says he's " + ageWithPartialYear);
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        }catch (NullPointerException e){
+            System.out.println(getInputFromScanner(currentYear));
+        }
+
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -24,7 +30,34 @@ public class Main {
     }
 
     public static String getInputFromScanner(int currentYear) {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi, What's your name? ");
+        String name = scanner.nextLine();
+        System.out.println("What year were you born? ");
+        boolean validDob = false;
+        int age = 0;
+        do {
+            System.out.println("Enter a year of birth >= " +
+                    (currentYear - 125 ) + "and <= " + currentYear );
+            try {
+                age = checkData(currentYear,scanner.nextLine());
+                validDob = age >= 0;
+            }catch (NumberFormatException badUserData){
+                System.out.println("Characters not allowed!!! try again");
+            }
+
+        }while (!validDob);
+
+        return "So you are " + age + " years old";
     }
 
+    public static int checkData(int currentYear, String dateOfBirth){
+       int dob = Integer.parseInt(dateOfBirth);
+       int minimumYear = currentYear - 125;
+
+       if(dob < minimumYear || dob > currentYear){
+          return -1;
+       }
+       return (currentYear - dob);
+    }
 }
