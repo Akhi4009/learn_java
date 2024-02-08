6a9e078e80de20c38a356c0f3ccf5381a61504a6
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-08 19:29:59 +0530

    merge sorted arrays

--- FindMissingNumber/src/missiingNumber.js
+++ FindMissingNumber/src/missiingNumber.js
@@ -30,6 +30,7 @@ function rotateArray(nums,k){
     return nums;
 }
 
+
 function reverse(nums,start,end){
     while(start < end){
         const temp = nums[start];
@@ -41,9 +42,31 @@ function reverse(nums,start,end){
     return arr;
 }
 
-// const array = [1,2,3,4,6,7];
-// console.log(missingNumber(array));
-const arr =[1,2,3,4,3]
-// console.log(findDuplicate(arr));
-// console.log(reverse(arr,0,arr.length-1))
-console.log(rotateArray(arr,2))
+
+function mergeSortedArrays(nums1,m,nums2,n){
+    let i = m-1
+    let j = n-1;
+    let k = m + n -1;
+    while (i >=0 && j>=0){
+      if(nums1[i] > nums2[j]){
+          nums1[k--] = nums1[i--];
+      }else {
+          nums1[k--] = nums2[j--];
+      }
+    }
+    while (j >= 0){
+        nums1[k--] = nums2[j--];
+    }
+    return nums1;
+}
+const arr1 =[1,2,3,4,5,6,8,9];
+const arr2 = [1,3,5,7,9,10,12]
+
+console.log(mergeSortedArrays(arr1,8,arr2,7))
+
+// // const array = [1,2,3,4,6,7];
+// // console.log(missingNumber(array));
+// const arr =[1,2,3,4,3]
+// // console.log(findDuplicate(arr));
+// // console.log(reverse(arr,0,arr.length-1))
+// console.log(rotateArray(arr,2))
