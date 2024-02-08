7ef304a7afda6fe803a9a835553649f1bab08fd0
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-08 13:23:45 +0530

    rotate array in java

--- FindMissingNumber/src/Main.java
+++ FindMissingNumber/src/Main.java
@@ -21,12 +21,36 @@ public class Main {
         }
         return -1; // Duplicate not found;
     }
+
+    public static  int[] rotateArray(int[] nums,int k){
+        k = k % nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+        return nums;
+    }
+
+    public static int[] reverse(int[] arr,int start, int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-    int[] arr = {1,2,3,3,4,5};
-        System.out.println(findDuplicate(arr));
+//    int[] arr = {1,2,3,3,4,5};
+//        System.out.println(findDuplicate(arr));
 
         int [] arr1 = {1,2,3,4,6};
-        System.out.println(findMissingNumber(arr1));
+        // 6, 4, 3, 2, 1
+//        System.out.println(findMissingNumber(arr1));
+        rotateArray(arr1,5);
+        for (int num : arr1 ){
+            System.out.print(num + " ");
+        }
 
     }
 }
