b24c19d277823f0f785be86b223722e8a5ed7ff6
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-08 19:41:36 +0530

    sorted arrays merge in typescript

--- FindMissingNumber/src/missingNumber.ts
+++ FindMissingNumber/src/missingNumber.ts
@@ -32,9 +32,32 @@ function reverse(arr:number[], start:number, end:number):number[]{
     }
     return arr;
 }
+
+function mergeSortedArrays(
+    nums1:number[],m:number,nums2:number[],n:number
+):number[]{
+
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
+    while (i >=0 && j >=0){
+        if(nums1[i] > nums2[j]){
+            nums1[k--] = nums1[i--]
+        }else{
+            nums1[k--] = nums2[j--]
+        }
+    }
+    while (j >=0){
+        nums1[k--] = nums2[j--];
+    }
+    return nums1;
+}
+
 const  arr1:number[] = [1,2,3,4,4,5]
+const arr2:number[] = [2,3,5,6,7]
+console.log(mergeSortedArrays(arr1,6,arr2,5))
 // console.log(findDuplicate(arr1));
 //
 // const arr = [1,2,3,4,5,7];
 // console.log(missingNumber(arr));
-console.log(rotateArray(arr1,3));
+// console.log(rotateArray(arr1,3));
