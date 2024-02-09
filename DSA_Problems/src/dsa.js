33cd41318bfcbba82be298f328526667a32f5476
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-09 16:54:06 +0530

    reverse number

--- DSA_Problems/src/dsa.js
+++ DSA_Problems/src/dsa.js
@@ -11,5 +11,25 @@ function twoSum(nums,target){
     return []
 }
 
-const arr = [1, 3, 4, 5, 6]
-console.log(twoSum(arr,7))
+function reverseInteger(x){
+    const INT_MAX = Math.pow(2,31) -1;
+    const INT_MIN = -Math.pow(2,31) ;
+
+    let reversed = 0;
+    while (x !== 0){
+        let digit = x % 10;
+        x = Math.trunc( x/10);
+        if(reversed > INT_MAX/10 || (reversed === INT_MAX/10 && digit > 7)) return 0;
+        if(reversed < INT_MIN/10 || (reversed === INT_MIN/10 && digit < -8)) return 0;
+
+        reversed = reversed*10 + digit;
+    }
+    return reversed;
+}
+
+console.log(reverseInteger(123));
+console.log(reverseInteger(-123));
+console.log(reverseInteger(120));
+//
+// const arr = [1, 3, 4, 5, 6]
+// console.log(twoSum(arr,7))
--- DSA_Problems/src/dsa.ts
+++ DSA_Problems/src/dsa.ts
@@ -12,5 +12,25 @@ function twoSum(nums:number[],target:number):number[]{
     return [];
 }
 
+function reverseInteger(x: number): number {
+    const INT_MAX: number = Math.pow(2, 31) - 1;
+    const INT_MIN: number = -Math.pow(2, 31);
+
+    let reversed: number = 0;
+    while (x !== 0) {
+        const digit: number = x % 10;
+        // @ts-ignore
+        x = Math.trunc(x / 10);
+        if (reversed > INT_MAX / 10 || (reversed === INT_MAX / 10 && digit > 7)) return 0;
+        if (reversed < INT_MIN / 10 || (reversed === INT_MIN / 10 && digit < -8)) return 0;
+        reversed = reversed * 10 + digit;
+    }
+    return reversed;
+}
+
+
+console.log(reverseInteger(123));
+console.log(reverseInteger(-123));
+console.log(reverseInteger(120));
 const arr = [1,3,3,4,5,6];
 console.log(twoSum(arr,4));
