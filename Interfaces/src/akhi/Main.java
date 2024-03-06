f8da07e70de7e0b290c31ae189c659c6fad25c8d
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-03-06 21:47:25 +0530

    interfaces

--- Interfaces/src/akhi/Main.java
+++ Interfaces/src/akhi/Main.java
@@ -8,11 +8,28 @@ public class Main {
         Trackable tracked = bird;
 
         animal.move();
+//        flier.takeOff();
+//        flier.fly();
+//        flier.land();
+//        tracked.track();
+        inFlight(flier);
+        inFlight(new Jet());
+
+        Trackable truck = new Truck();
+        truck.track();
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n",
+                kmsTraveled, milesTraveled);
+    }
+    private static void inFlight(FlightEnabled flier){
         flier.takeOff();
         flier.fly();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
         flier.land();
-        tracked.track();
-
     }
 
 }
